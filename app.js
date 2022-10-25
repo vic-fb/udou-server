a1a0fcb7548c279fe2a5ce1c05fa5a3cfa734f19
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-10-25 11:03:07 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const cors = require('cors');  // add at the top
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+
+
+app.use(cors());  // add after 'app' is created
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
