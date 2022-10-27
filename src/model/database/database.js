676c653137dab99b0cabe7f3c11f9b5e21425ac1
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-10-27 08:46:11 +0200

    feat(database): Create database

---
+++ src/model/database/database.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const mysql = require('mysql');
+const fs = require('fs');
+
+const { DB_HOST } = process.env;
+const { DB_USER } = process.env;
+const { DB_PASS } = process.env;
+const { DB_NAME } = process.env;
+
+const con = mysql.createConnection({
+  host: DB_HOST || '127.0.0.1',
+  user: DB_USER || 'root',
+  password: DB_PASS,
+  database: DB_NAME,
+  multipleStatements: true,
+});
+
+con.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+
+  const sql = fs.readFileSync(`${__dirname}/init_db.sql`).toString();
+  con.query(sql, (error) => {
+    if (error) throw error;
+    console.log('Tables creation was successful!');
+    console.log('Closing...');
+  });
+
+  con.end();
+});
