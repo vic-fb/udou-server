6a7df8d195a61bd498472429bb326c4ac8aac6e1
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-10-28 17:51:32 +0200

    feat(app): Update app routes

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const cors = require('cors');  // add at the top
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const usersRouter = require('./src/controller/users');
+const trackablesRouter = require('./src/controller/trackables');
+const entriesRouter = require('./src/controller/entries');
 
 const app = express();
 
-
-
-app.use(cors());  // add after 'app' is created
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/trackables', trackablesRouter);
+app.use('/entries', entriesRouter);
 
 module.exports = app;
