f2b0c508a1991c0b556e7c74c555346ed0563156
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-10-28 17:49:54 +0200

    feat(database): Change entries tables

--- src/model/database/init_db.sql
+++ src/model/database/init_db.sql
@@ -1,8 +1,7 @@
 SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS `users`;
 DROP TABLE IF EXISTS `trackables`;
-DROP TABLE IF EXISTS `boolean_entries`;
-DROP TABLE IF EXISTS `quantitative_entries`;
+DROP TABLE IF EXISTS `entries`;
 SET foreign_key_checks = 1;
 
 CREATE TABLE `users`(
@@ -22,26 +21,18 @@ CREATE TABLE `trackables`(
     `active` TINYINT(1) NOT NULL
 );
 
-CREATE TABLE `boolean_entries`(
+CREATE TABLE `entries`(
     `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `trackable_id` INT UNSIGNED NOT NULL,
     `date` DATE NOT NULL,
-    `value` TINYINT(1) NOT NULL
-);
-
-CREATE TABLE `quantitative_entries`(
-    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `trackable_id` INT UNSIGNED NOT NULL,
-    `date` DATE NOT NULL,
-    `value` DOUBLE(8, 2) NOT NULL
+    `boolean_value` TINYINT(1) NULL,
+    `quantitative_value` DOUBLE(8, 2) NULL
 );
 
 ALTER TABLE
     `trackables` ADD CONSTRAINT `trackables_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
 ALTER TABLE
-    `boolean_entries` ADD CONSTRAINT `boolean_entries_trackable_id_foreign` FOREIGN KEY(`trackable_id`) REFERENCES `trackables`(`id`);
-ALTER TABLE
-    `quantitative_entries` ADD CONSTRAINT `quantitative_entries_trackable_id_foreign` FOREIGN KEY(`trackable_id`) REFERENCES `trackables`(`id`);
+    `entries` ADD CONSTRAINT `boolean_entries_trackable_id_foreign` FOREIGN KEY(`trackable_id`) REFERENCES `trackables`(`id`);
 
 INSERT INTO users (name, email)
 VALUES
@@ -57,18 +48,15 @@ VALUES
     (1, "Waist", "quantitative", "cm", "yellow", "any", 1),
     (1, "Bloating", "boolean", null, "blue", "any", 1);
 
-INSERT INTO quantitative_entries (trackable_id, date, value)
+INSERT INTO entries (trackable_id, date, boolean_value, quantitative_value)
 VALUES
-    (2, '2022-10-26' , 75.0),
-    (3, '2022-10-26', 68),
-    (2, '2022-10-31', 75.4),
-    (3, '2022-10-31', 67.5);
-
-INSERT INTO boolean_entries (trackable_id, date, value)
-VALUES
-    (1, '2022-10-26' , 1),
-    (3, '2022-10-26', 1),
-    (4, '2022-10-26', 1),
-    (6, '2022-10-27', 1),
-    (1, '2022-10-27', 0),
-    (4, '2022-10-27', 0);
+    (2, '2022-10-26' , null, 75.0),
+    (3, '2022-10-26', null, 68),
+    (2, '2022-10-31', null, 75.4),
+    (3, '2022-10-31', null, 67.5),
+    (1, '2022-10-26', 1, null),
+    (3, '2022-10-26', 1, null),
+    (4, '2022-10-26', 1, null),
+    (6, '2022-10-27', 1, null),
+    (1, '2022-10-27', 0, null),
+    (4, '2022-10-27', 0, null);
